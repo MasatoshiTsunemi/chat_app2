8fed7e53325ab3b655fb86d87e66da15e35846d4
m-tsunemi <m-tsunemi@sample.com>
2019-09-27 18:50:45 +0900

    first

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,52 @@
+class RoomsController < ApplicationController
+
+  # Deviseのログイン確認
+  before_action :authenticate_user!
+
+  # Room一覧アクション
+  def index
+    @rooms = Room.all.order(:id)
+  end
+
+  def show
+    @room = Room.find(params[:id])
+    # メッセージ一覧を取得
+    @messages = @room.messages
+  end
+
+  def new
+    @room = Room.new
+  end
+  
+  def create    
+    puts
+    @room = Room.new(name: params[:name])
+
+    if @room.save
+      redirect_to root_path, notice: "新たな部屋を作成しました"
+    else
+      render rooms_new_path
+    end
+  end
+
+  def edit
+    @room = Room.find_by(id: params[:id])
+  end
+
+  def update
+    @room = Room.find_by(id: params[:id])
+    @room.assign_attributes(name: params[:name])
+    if @room.save
+      redirect_to root_path, notice: "部屋名を変更しました"
+    else
+      render rooms_edit_path
+    end
+  end
+
+  def destroy
+    @room = Room.find(params[:id])
+    @room.destroy
+    redirect_to root_path, notice: "部屋を削除しました"
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # get 'rooms/new' => 'rooms#new'
+  # get 'rooms/:id/edit' => 'rooms#edit'
+  # post 'rooms/create' => 'rooms#create'
+  # post 'rooms/:id/update' => 'rooms#update'
+  resources :rooms
+  # delete 'rooms/:id/destroy' => 'rooms#destroy'
+  devise_for :users
+  root 'rooms#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
