d188aff8ff4dc65c79dbf954a78f348e9e456c26
HirokiTomisawa <htomisawa0602@gmail.com>
2019-09-28 21:38:57 +0900

    modify migration file

--- db/schema.rb
+++ db/schema.rb
@@ -36,8 +36,9 @@ ActiveRecord::Schema.define(version: 2019_09_27_103447) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "username"
+    t.string "username", default: "", null: false
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
